13c94c7b94831385c6435ed0f9a93ae2f4fa4fc7
rurre <sergiuung@msn.com>
2025-06-18 06:43:53 +0300

    Changed social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/shiny-cat-transparent.png',
     navbar: {
       title: "Pumkin",
       logo: {
