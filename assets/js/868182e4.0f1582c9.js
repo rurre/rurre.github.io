5749de2a4c354c96d2ab44d1d4bdbd74c0d4d623
rurre <16716633+rurre@users.noreply.github.com>
2026-02-07 21:14:07 +0200

    Deploy website - based on e5894cef18696390fa47c5281aa723111e0adbc4

--- assets/js/868182e4.0f1582c9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrurre_github_io=self.webpackChunkrurre_github_io||[]).push([[3517],{9288:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/updates/page/2","page":2,"postsPerPage":10,"totalPages":2,"totalCount":20,"previousPage":"/updates","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
