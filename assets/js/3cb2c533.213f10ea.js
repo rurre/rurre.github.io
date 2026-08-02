2658ee8e839518d4695ed6a4f2e0c35d9ec5ad7d
rurre <16716633+rurre@users.noreply.github.com>
2026-08-02 08:12:07 +0300

    Deploy website - based on 356fe88ca8e214a5e5eb85e5f01290e0dc099fa9

--- assets/js/3cb2c533.213f10ea.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrurre_github_io=self.webpackChunkrurre_github_io||[]).push([[447],{9677:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/updates/page/4","page":4,"postsPerPage":10,"totalPages":4,"totalCount":35,"previousPage":"/updates/page/3","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
